3adec7216fcbc7f745d44a93ee980e7a05774856
Thomas Lamisse <tom.lamisse@gmail.com>
2024-10-28 12:06:45 +0100

    ajout de tv

---
+++ src/components/TV/TV.tsx
@@ -0,0 +1,12 @@
+import "./TV.css"
+
+function TV(){
+    return (
+<div>
+    <a href="https://www.tf1.fr/" target="_blank" rel="noreferrer"><img src="pngtree-flat-screen-tv-transparent-png-free-clipart-png-image_5190594-removebg-preview.png" alt=""  /></a>
+    
+
+</div>
+    )
+}
+export default TV
