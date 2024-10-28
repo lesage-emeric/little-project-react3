311eca8ec8c9d8c79ac4d85b869e644007d41e46
Emeric Lesage <elesage.dev@gmail.com>
2024-10-28 15:14:59 +0100

    added img for objects array

--- src/components/Appartment.tsx
+++ src/components/Appartment.tsx
@@ -1,42 +1,42 @@
 const Appartment = () => {
-  const objects = [
-    {
-      name: "TV",
-      imgSrc:
-        "public/pngtree-flat-screen-tv-transparent-png-free-clipart-png-image_5190594-removebg-preview.png",
-      url: "https://www.tf1.fr/",
-    },
-    {
-      name: "Counch",
-      imgSrc: "public/imgCounch.png",
-      url: "https://www.ikea.com/fr/fr/",
-    },
-    {
-      name: "Kitchen",
-      imgSrc: "",
-      url: "https://www.chronodrive.com/",
-    },
-    {
-      name: "Table",
-      imgSrc: "",
-      url: "https://www.lejardindesfleurs.com/",
-    },
-    {
-      name: "Bookshelf",
-      imgSrc: "",
-      url: "https://wwww.amazon.com/",
-    },
-  ];
+	const objects = [
+		{
+			name: "TV",
+			imgSrc:
+				"public/pngtree-flat-screen-tv-transparent-png-free-clipart-png-image_5190594-removebg-preview.png",
+			url: "https://www.tf1.fr/",
+		},
+		{
+			name: "Counch",
+			imgSrc: "public/imgCounch.png",
+			url: "https://www.ikea.com/fr/fr/",
+		},
+		{
+			name: "Kitchen",
+			imgSrc: "public/imgFridge.png",
+			url: "https://www.chronodrive.com/",
+		},
+		{
+			name: "Table",
+			imgSrc: "public/imgFlower.png",
+			url: "https://www.lejardindesfleurs.com/",
+		},
+		{
+			name: "Bookshelf",
+			imgSrc: "public/imgBookshelf.png",
+			url: "https://wwww.amazon.com/",
+		},
+	];
 
-  return (
-    <>
-      {objects.map((element) => (
-        <a href={element.url}>
-          <img src={element.imgSrc} alt={element.name} />
-        </a>
-      ))}
-    </>
-  );
+	return (
+		<>
+			{objects.map((element) => (
+				<a href={element.url}>
+					<img src={element.imgSrc} alt={element.name} />
+				</a>
+			))}
+		</>
+	);
 };
 
 export default Appartment;
