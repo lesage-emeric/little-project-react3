adf4829b3fe35135c51bbebf4b81556851e54194
Thomas Lamisse <tom.lamisse@gmail.com>
2024-10-28 14:11:54 +0100

    tableau avatar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,28 @@ import Counch from "./components/Counch/Counch";
 import NavBar from "./components/NavBar/NavBar";
 import TV from "./components/TV/TV"
 
+const Friends= [
+  {
+    FirstName : "Monica" ,
+    img : ""
+  },
+  {
+    FirstName : "Joey" ,
+    img : ""
+  },{
+    FirstName : "Chandler" ,
+    img : ""
+  },{
+    FirstName : "Phoebe" ,
+    img : ""
+  },{
+    FirstName : "Ross" ,
+    img : ""
+  },{
+    FirstName : "Rachel" ,
+    img : ""
+  },
+]
 function App() {
 	return (
 		<>
